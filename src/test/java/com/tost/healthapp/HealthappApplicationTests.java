d690ed4f56352e721e85c63088a5bbed5058b4b3
Sajid Haniff <sajid.haniff@gmail.com>
2020-07-18 00:23:40 -0400

    updated git ignore

---
+++ src/test/java/com/tost/healthapp/HealthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tost.healthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
